7777e76f46b472c4fb138a25b6b7aa416474bfa3
Krug <mribeirokruger@gmail.com>
2026-03-26 22:44:35 -0300

    Update customize.sh

--- module/customize.sh
+++ module/customize.sh
@@ -1,23 +1,9 @@
-ui_print ""
-ui_print "- Select cursor style:"
-ui_print "  Volume+  =  Dark cursor (white outline) [DEFAULT]"
-ui_print "  Volume-  =  Light cursor (dark outline)"
-ui_print ""
+ui_print "- Installing Input Device Config (.idc)"
+ui_print "- Injecting Resource Overlay (RRO)"
+ui_print "- Installation Complete!"
 
-# Default to dark
-cp "$MODPATH/variants/dark.apk" "$MODPATH/system/product/overlay/SPenCursor.apk"
-
-# Wait for Volume- to switch, timeout after 10s
-for i in $(seq 1 10); do
-    getevent -lc 1 2>/dev/null | grep -q "KEY_VOLUMEDOWN" && {
-        ui_print "- Selected: Light"
-        cp "$MODPATH/variants/light.apk" "$MODPATH/system/product/overlay/SPenCursor.apk"
-        break
-    }
-    getevent -lc 1 2>/dev/null | grep -q "KEY_VOLUMEUP" && {
-        ui_print "- Selected: Dark (default)"
-        break
-    }
-done
-
-ui_print "- Cursor style set!"
+ui_print " "
+ui_print "  Go to Developer Options and"
+ui_print "  ENABLE 'Show Taps' to see the"
+ui_print "  S-Pen hover pointer."
+ui_print " "
