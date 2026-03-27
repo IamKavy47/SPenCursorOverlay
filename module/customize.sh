d440f3080d84b7f32a4fdc61cb2557321fd36b53
Krug <mribeirokruger@gmail.com>
2026-03-26 23:37:59 -0300

    fix file permissions

--- module/customize.sh
+++ module/customize.sh
@@ -7,3 +7,6 @@ ui_print "  Go to Developer Options and"
 ui_print "  ENABLE 'Show Taps' to see the"
 ui_print "  S-Pen hover pointer."
 ui_print " "
+
+set_perm_recursive $MODPATH/system/usr/idc 0 0 0755 0644
+set_perm_recursive $MODPATH/system/product/overlay 0 0 0755 0644
