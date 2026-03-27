bafe62bf152512b2c12f1446dd640e5f33bbef4c
Krug <mribeirokruger@gmail.com>
2026-03-27 01:52:55 -0300

    v1.7

--- module/customize.sh
+++ module/customize.sh
@@ -1,18 +1,21 @@
 #!/system/bin/sh
 
 get_input() {
-  timeout 1s /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME
+  timeout 1s /system/bin/getevent -lc 1 2>&1 | grep VOLUME
 }
 
 ui_print " "
-ui_print "  Select Cursor Theme:"
-ui_print "   Vol+ = Light Cursor (White)"
-ui_print "   Vol- = Dark Cursor (Black)"
+ui_print "  --- Select Cursor Color ---"
+ui_print "  Vol+ = Light Theme"
+ui_print "  Vol- = Dark Theme"
+ui_print " "
+ui_print "  Waiting 30s for input..."
+ui_print "  (Defaults to Dark if no keys pressed)"
 ui_print " "
 
 COUNT=0
 SELECTED="none"
-while [ $COUNT -lt 3600 ]; do
+while [ $COUNT -lt 30 ]; do
   EVENT=$(get_input)
   if echo "$EVENT" | grep -q "KEY_VOLUMEUP"; then
     SELECTED="light"
@@ -24,25 +27,39 @@ while [ $COUNT -lt 3600 ]; do
   COUNT=$((COUNT + 1))
 done
 
-mkdir -p "$MODPATH/system/product/overlay"
+TARGET_DIR="$MODPATH/system/product/overlay"
+mkdir -p "$TARGET_DIR"
 
 if [ "$SELECTED" = "light" ]; then
+  FILE="LightCursor.apk"
   ui_print "- Selected: Light Theme"
-  cp -f "$MODPATH/common/LightCursor.apk" "$MODPATH/system/product/overlay/CursorOverlay.apk"
 else
+  FILE="DarkCursor.apk"
   [ "$SELECTED" = "none" ] && ui_print "- Timeout: Defaulting to Dark Theme" || ui_print "- Selected: Dark Theme"
-  cp -f "$MODPATH/common/DarkCursor.apk" "$MODPATH/system/product/overlay/CursorOverlay.apk"
 fi
+sleep 1.0
+
+if [ -f "$MODPATH/common/$FILE" ]; then
+  cp -f "$MODPATH/common/$FILE" "$TARGET_DIR/CursorOverlay.apk"
+else
+  ui_print "! ERROR: Source $FILE not found in /common/"
+fi
+
 rm -rf "$MODPATH/common"
 
 ui_print "- Installing Input Device Config..."
+sleep 0.2
 ui_print "- Injecting Resource Overlay..."
+sleep 0.2
+
+[ -d "$MODPATH/system/usr/idc" ] && set_perm_recursive "$MODPATH/system/usr/idc" 0 0 0755 0644
+set_perm "$TARGET_DIR/CursorOverlay.apk" 0 0 0644
+
 ui_print "- Installation Complete!"
+sleep 0.5
+
 ui_print " "
 ui_print "  Go to Developer Options and"
-ui_print "  ENABLE Show Taps to see the"
+ui_print "  ENABLE 'Show taps' to see the"
 ui_print "  S-Pen hover pointer."
 ui_print " "
-
-set_perm_recursive "$MODPATH/system/usr/idc" 0 0 0755 0644
-set_perm "$MODPATH/system/product/overlay/CursorOverlay.apk" 0 0 0644
